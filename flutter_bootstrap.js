433c760df4734b1627c0c8768f2105747eb730c3
Rom4ik-r <jitov.roman@yandex.ru>
2025-03-29 13:43:41 +0300

    mysaytec

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3656894944"
+    serviceWorkerVersion: "1864240080"
   }
 });
